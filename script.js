016071e279e61586811a3ae290b396230f5be2b0
Mubbashirkhan87 <mubbashirkhan8787505@gmail.com>
2025-02-11 02:10:10 -0800

    js3

---
+++ script.js
@@ -0,0 +1,38 @@
+// groosry milk list
+let grosry = [];
+grosry.push ("Milk", "Eggs", "Bread")
+grosry.pop(2)
+grosry.unshift("Friuts")
+grosry.shift(0)
+
+console.log(grosry);
+
+
+//student enrollment system
+let enrollment = ["John", "Emma", "Noah", "Liam"]
+enrollment.pop(3)
+enrollment.splice(2)
+enrollment.push("Sophia")
+
+console.log(enrollment);
+
+// Movies 
+let movies = ["Inception", "Avatar", "Titanic", "Joker", "Interstellar"] 
+movies.splice(3)
+console.log(movies)
+
+//2nd
+let movie = ["Inception", "Avatar", "Titanic", "Joker", "Interstellar"] 
+movie.splice(3,1)
+movie.splice(2,0,"the Matrix")
+console.log(movie)
+
+// student
+let student = ["Alice", "Bob", "Charlie"]
+student.push("David")
+student.unshift("Emma")
+student.shift()
+console.log(student)
+
+
+//complete the js 4 assigment 
